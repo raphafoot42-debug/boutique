7dcb97c464eda453550fb31947de359a67ec23eb
raphael AUDONNET <raphafoot42@gmail.com>
2026-06-06 17:47:24 +0200

    Update products.ts

--- tshirt-store/lib/products.ts
+++ tshirt-store/lib/products.ts
@@ -15,7 +15,7 @@ export const products: Product[] = [
     name: 'NexaWear T-shirt',
     description: 'T-shirt de qualité premium, coupe moderne. Disponible en plusieurs tailles.',
     price: 1999,
-    image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=600&q=80',
+    image: 'https://i.imgur.com/9fKln7N.jpg',
     sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
     stripePriceId: 'price_1TebF7LyTbnMrw2ZcIhO4CBe',
     colors: ['Noir', 'Blanc'],
