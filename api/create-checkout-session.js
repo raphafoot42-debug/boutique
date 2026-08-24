bf36cf0dacf9f13cba24a0415d9a88c6db544d53
raphael AUDONNET <raphafoot42@gmail.com>
2026-08-24 19:05:33 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-11-20",
+  apiVersion: "2026-07-29.dahlia",
 });
 
 export default async function handler(req, res) {
